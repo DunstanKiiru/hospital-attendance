6aceabd6759372de5bfb135f033ec23f8c5435b0
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-08-04 19:12:30 +0300

    Adds middleware to app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\EmployeeMiddleware;
+use App\Http\Middleware\HRMiddleware;
+use App\Http\Middleware\SupervisorMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' =>AdminMiddleware::class,
+            'HR' => HRMiddleware::class,
+            'supervisor' => SupervisorMiddleware::class,
+            'employee' => EmployeeMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
